46f3fd02422ef420b714a22cd0040135733456f3
Guillaume BADINIER <gbadinier@saretec.fr>
2021-08-25 17:05:05 +0200

    Add configuration for use with swashbuckle

--- Clients.cs
+++ Clients.cs
@@ -9,9 +9,12 @@ internal class Clients
             new Client {
                 ClientId = "oauthClient",
                 ClientName = "Example client application using client credentials",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = false,
                 ClientSecrets = new List<Secret> { new Secret("password".Sha256())},
-                AllowedScopes = new List<string> { "api1.read"}
+                AllowedScopes = new List<string> { "api1.read"},
+                RedirectUris = {"https://localhost:5002/swagger/oauth2-redirect.html"},
+                AllowedCorsOrigins = {"https://localhost:5002"},
             }
         };
     }
